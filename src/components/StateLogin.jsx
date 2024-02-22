4447a9d371b834727b4de49399d98f848505e66f
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-02-21 21:55:26 -0400

    Validating email on every Keystroke via State

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,6 +8,9 @@ export default function Login() {
     password: '',
   });
 
+  const emailIsInvalid =
+    enteredValues.email !== '' && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log('Submission', enteredValues);
@@ -44,6 +47,9 @@ export default function Login() {
             }}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
