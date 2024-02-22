47f97573823c7c44281f46f313717f948a1ff54d
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-02-21 21:03:29 -0400

    Manage and get user input via individual state and generic handler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState();
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('Submission');
+    console.log('Submission', enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
+
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setEnteredPassword(event.target.value);
   }
 
   return (
@@ -11,12 +35,28 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => {
+              handleInputChange('email', event.target.value);
+            }}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => {
+              handleInputChange('password', event.target.value);
+            }}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
